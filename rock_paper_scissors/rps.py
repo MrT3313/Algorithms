ff60a5f0d1aeea0a2a0fed535443019905125ede
Mr.T <rturgeon13@gmail.com>
2019-12-13 13:11:19 -0600

    Rock Paper Scissors - instructor hour solution

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,8 +2,29 @@
 
 import sys
 
+def recurse(prev, n, all_results):
+  #Base Case
+  if n == 0:
+    all_results.append(prev)
+    return
+  
+  #Recursive Step
+  recurse(prev + ['R'], n-1, all_results)
+  recurse(prev + ['P'], n-1, all_results)
+  recurse(prev + ['S'], n-1, all_results)
+
 def rock_paper_scissors(n):
-  pass 
+
+  results = []
+  recurse([], n, results)
+
+  print(results)
+  return results
+
+
+# rock_paper_scissors(2)
+rock_paper_scissors(3)
+
 
 
 if __name__ == "__main__":
